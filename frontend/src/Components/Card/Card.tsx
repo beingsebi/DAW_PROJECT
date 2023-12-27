60ffec3a14a2f076a47d823af48dce38587c6218
Popa Sebastian <sebastianpopa208@gmail.com>
2023-12-27 19:24:01 +0200

    add portfolio button and a bit of refactoring

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import "./Card.css";
 import { CompanySearch } from "../../interfaces";
+import AddPortfolio from "../Portfolio/AddPortfolio/AddPortfolio";
 interface Props {
   id: string;
   searchResult: CompanySearch;
+  onPortfolioCreate: (e: SyntheticEvent) => void;
 }
 
-const Card: React.FC<Props> = ({ id, searchResult }: Props): JSX.Element => {
+const Card: React.FC<Props> = ({
+  id,
+  searchResult,
+  onPortfolioCreate,
+}: Props): JSX.Element => {
   return (
     <div className="card">
       <img
@@ -22,6 +28,10 @@ const Card: React.FC<Props> = ({ id, searchResult }: Props): JSX.Element => {
       <p className="info">
         {searchResult.exchangeShortName} - {searchResult.stockExchange}
       </p>
+      <AddPortfolio
+        onPortfolioCreate={onPortfolioCreate}
+        symbol={searchResult.symbol}
+      />
     </div>
   );
 };
