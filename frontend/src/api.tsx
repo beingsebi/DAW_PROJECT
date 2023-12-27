cd7900ef841f5f45e82e807bc6583aae645afa15
Popa Sebastian <sebastianpopa208@gmail.com>
2023-12-27 23:17:21 +0200

    Company profile

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CompanyProfile, CompanySearch } from "./interfaces";
+import { CompanyKeyMetrics, CompanyProfile, CompanySearch } from "./interfaces";
 interface SearchResponse {
   data: CompanySearch[];
 }
@@ -24,6 +24,17 @@ export const getCompanyProfile = async (query: string) => {
   try {
     const data = await axios.get<CompanyProfile[]>(
       `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error message: ", error.message);
+  }
+};
+
+export const getKeyMetrics = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyKeyMetrics[]>(
+      `https://financialmodelingprep.com/api/v3/key-metrics-ttm/${query}?limit=40&apikey=${process.env.REACT_APP_API_KEY}`
     );
     return data;
   } catch (error: any) {
