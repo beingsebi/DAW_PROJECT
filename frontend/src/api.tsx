d482843d561f92e2eda5b53d86f254948ed4d85d
Popa Sebastian <sebastianpopa208@gmail.com>
2023-12-27 17:39:17 +0200

    add api prep for financial data

---
+++ frontend/src/api.tsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { CompanySearch } from "./interfaces";
+interface SearchResponse {
+  data: CompanySearch[];
+}
+export const searchCompanies = async (query: string) => {
+  try {
+    const data = await axios.get<SearchResponse>(
+      `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    console.log("EEEEE");
+    console.log(process.env.API_KEY);
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log(error.message);
+      return error.message;
+    } else {
+      console.log("unexpected error", error);
+      return "unexpected error";
+    }
+  }
+};
