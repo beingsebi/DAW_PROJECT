b1ce440afeaa0ff0d458f7321ad0a930125b932e
Popa Sebastian <sebastianpopa208@gmail.com>
2023-12-27 22:38:51 +0200

    Company page and dashboard

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
-import { CompanyProfile } from "../../interfaces";
+import { Link, useParams } from "react-router-dom";
 import { getCompanyProfile } from "../../api";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import CompanyDashboard from "../../Components/CompanyDashboard/CompanyDashboard";
+import { CompanyProfile } from "../../interfaces";
+import Tile from "../../Components/Tile/Tile";
+import Spinner from "../../Components/Spinner/Spinner";
 
 interface Props {}
 
 const CompanyPage = (props: Props) => {
   let { ticker } = useParams();
+
   const [company, setCompany] = useState<CompanyProfile>();
 
   useEffect(() => {
@@ -16,12 +21,26 @@ const CompanyPage = (props: Props) => {
     };
     getProfileInit();
   }, []);
+
   return (
     <>
       {company ? (
-        <div>{company.companyName}</div>
+        <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+          <Sidebar />
+          <CompanyDashboard ticker={ticker!}>
+            <Tile title="Company Name" subTitle={company.companyName} />
+            <Tile title="Price" subTitle={"$" + company.price.toString()} />
+            <Tile title="DCF" subTitle={"$" + company.dcf.toString()} />
+            <Tile title="Sector" subTitle={company.sector} />
+            {/* <CompFinder ticker={company.symbol} />
+            <TenKFinder ticker={company.symbol} /> */}
+            <p className="bg-white shadow rounded text-medium font-medium text-gray-900 p-3 mt-1 m-4">
+              {company.description}
+            </p>
+          </CompanyDashboard>
+        </div>
       ) : (
-        <div>Company not found!</div>
+        <Spinner />
       )}
     </>
   );
