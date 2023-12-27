ac4ad147397c75aaba62fe0035b049762090be84
Popa Sebastian <sebastianpopa208@gmail.com>
2023-12-27 23:24:19 +0200

    income statement

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,5 +1,10 @@
 import axios from "axios";
-import { CompanyKeyMetrics, CompanyProfile, CompanySearch } from "./interfaces";
+import {
+  CompanyIncomeStatement,
+  CompanyKeyMetrics,
+  CompanyProfile,
+  CompanySearch,
+} from "./interfaces";
 interface SearchResponse {
   data: CompanySearch[];
 }
@@ -35,6 +40,17 @@ export const getKeyMetrics = async (query: string) => {
   try {
     const data = await axios.get<CompanyKeyMetrics[]>(
       `https://financialmodelingprep.com/api/v3/key-metrics-ttm/${query}?limit=40&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error message: ", error.message);
+  }
+};
+
+export const getIncomeStatement = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyIncomeStatement[]>(
+      `https://financialmodelingprep.com/api/v3/income-statement/${query}?limit=50&apikey=${process.env.REACT_APP_API_KEY}`
     );
     return data;
   } catch (error: any) {
