7f4df9b3e93ae34107bfaddbd5b140b1b4ee936e
Popa Sebastian <sebastianpopa208@gmail.com>
2023-12-29 15:28:58 +0200

    add dto

--- backend/Controllers/StockController.cs
+++ backend/Controllers/StockController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using backend.Data;
+using backend.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers
@@ -21,7 +22,7 @@ namespace backend.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var stocks = _context.Stocks.ToList();
+            var stocks = _context.Stocks.ToList().Select(s => s.ToStockDto());
             return Ok(stocks);
         }
 
@@ -33,7 +34,7 @@ namespace backend.Controllers
             {
                 return NotFound();
             }
-            return Ok(stock);
+            return Ok(stock.ToStockDto());
         }
 
 
